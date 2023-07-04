f077e68903c7e49c9ece731286ba2bbc6020382a
bcarvalho99 <bcarvalho5699@gmail.com>
2023-07-04 18:22:38 -0300

    first step

---
+++ src/main/java/com/spookyAPI/spooky/SpookyApplication.java
@@ -0,0 +1,13 @@
+package com.spookyAPI.spooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpookyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spookyAPI/spooky/SpookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spookyAPI.spooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpookyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
